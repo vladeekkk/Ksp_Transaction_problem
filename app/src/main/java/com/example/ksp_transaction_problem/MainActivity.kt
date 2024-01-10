c0be30d231eeb254a20acd2b450e0d8ba816f6e8
vladeekkk <vlad.krasss@yandex.ru>
2024-01-10 09:35:55 +0300

    update room/kotlin/ksp/gradle versions to latest

--- app/src/main/java/com/example/ksp_transaction_problem/MainActivity.kt
+++ app/src/main/java/com/example/ksp_transaction_problem/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.ksp_transaction_problem
 import android.app.Activity
 import android.os.Bundle
 import com.example.ksp_transaction_problem.database.DatabaseWithDaos
-import com.example.ksp_transaction_problem.database.MyDatabase
 import com.example.ksp_transaction_problem.database.MyDatabase_Impl
 
 class MainActivity: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // remove comment below and also get an error
-        val db = MyDatabase_Impl() //as DatabaseWithDaos
+        // remove cast to DatabaseWithDao to get rid of error in KSP
+        val db = MyDatabase_Impl() as DatabaseWithDaos
 
         db.runInTransaction(Runnable {
             // do stuff with multiple DAO's in ONE transaction
